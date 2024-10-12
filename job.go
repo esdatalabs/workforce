79b6f2b3c536179c4c58e888241772ea7ed6106d
Kyron Stubbs <kstubbs04@gmail.com>
2024-10-12 19:05:48 +0000

    added constructor and execute methods

--- job.go
+++ job.go
@@ -3,11 +3,42 @@ package workforce
 import "context"
 
 // The ExecuteFunction is the single unit of work to be performed
-type ExecuteFunction func(ctx context.Context, args ...interface{}) (interface{}, error)
+type ExecuteFunction func(ctx context.Context, args interface{}) (interface{}, error)
 
 // A job has the fields required to submit a unit of work to the worker pool
 type Job struct {
 	Description string
 	Arguments   interface{}
 	executable  ExecuteFunction
+}
+
+// A result has the fields required to describe the output of a job being executed
+type Result struct {
+	Description string
+	Value       interface{}
+	Err         error
+}
+
+// Helper method to construct a new Job
+func NewJob(desc string, args interface{}, exec ExecuteFunction) Job {
+	return Job{
+		Description: desc,
+		Arguments:   args,
+		executable:  exec,
+	}
+}
+
+// The Execute function is triggered by a worker in worker pool
+func (j *Job) Execute(ctx context.Context) Result {
+
+	value, err := j.executable(ctx, j.Arguments)
+
+	res := Result{Description: j.Description, Value: value}
+
+	if err != nil {
+		res.Err = err
+	}
+
+	return res
+
 }
